fcf845e86494bba5eba62620e2c0a4a07af94d48
VitorVLima <vitor14_df@hotmail.com>
2025-05-18 16:50:29 -0300

    att_tabela_alunos

--- app/model/alunos.py
+++ app/model/alunos.py
@@ -10,6 +10,7 @@ class Aluno (db.Model):
     cidade = db.Column(db.String(50), nullable = True)
     estado = db.Column(db.String(2), nullable = True)
     telefone = db.Column(db.String(15), nullable = True, unique=True)
+    status = db.Column(db.String(40), nullable = True, default='cadastrado')
     data_matricula = db.Column(db.Date, nullable = False)
     data_vencimento = db.Column(db.Date, nullable = False)
     data_desligamento = db.Column(db.Date, nullable = False)
