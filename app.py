3cb0e6749451d9c47ec8adaf7adf9b329e3bb8ae
VitorVLima <vitor14_df@hotmail.com>
2025-05-18 18:07:42 -0300

    att_tabela_alunos

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from app import enviroment
+from app import enviroment, db
+from app.model.alunos import Aluno
+
+with enviroment.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     enviroment.run(debug=True, host="127.0.0.1", port="8080")
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 enviroment = Flask(__name__) 
 #Definir URI do banco de dados
 enviroment.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crud.db"
+enviroment.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(enviroment)
 
