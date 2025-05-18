4cd9de0201cdfe0c7337ab35c1aa5fd150646c9d
Igor Stenio <160430256+Igormeu@users.noreply.github.com>
2025-05-18 01:19:01 -0300

    Modelo

---
+++ app.py
@@ -0,0 +1,4 @@
+from app import enviroment
+
+if __name__ == "__main__":
+    enviroment.run(debug=True, host="127.0.0.1", port="8080")
---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+enviroment = Flask(__name__) 
+#Definir URI do banco de dados
+enviroment.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crud.db"
+
+db = SQLAlchemy(enviroment)
+
+
+@enviroment.route("/") #Decorador python
+def index ():
+    return render_template("index.html")
+
+@enviroment.route("/newRequest") #Decorador python
+def request ():
+    return "<h1> Say me what is your new request:  </h1>"
